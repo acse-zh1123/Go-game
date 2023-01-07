4dd0b07344070d58eb188a886c0009efe0729c57
onelike21 <66159987+onelike21@users.noreply.github.com>
2023-01-07 20:24:40 +0000

    Update Go.sol

--- Go.sol
+++ Go.sol
@@ -24,4 +24,31 @@ contract Go is AbstractGo {
   function getBoard() public view returns (uint8[19][19]) {
     return board;
   }
+  
+  // The player's status. 0 for inactive, 1 for active.
+  uint8 public status;
+
+  // Makes a move on the board.
+  function makeMove(uint8 x, uint8 y) public {
+    require(validateMove(x, y), "Invalid move.");
+    applyMove(x, y);
+    emit MoveMade(x, y, turn);
+    turn = turn == 1 ? 2 : 1;
+    // Update the player's status.
+    if (turn == 1) {
+      status = 1;
+    } else {
+      status = 0;
+    }
+  }
+
+  // Sets the player's status to inactive.
+  function setInactive() public {
+    status = 0;
+  }
+
+  // Sets the player's status to active.
+  function setActive() public {
+    status = 1;
+  }
 }
